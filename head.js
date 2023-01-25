52e79502367f77e510e95892f52669e73d6ab1b9
Oscar <omankiewicz77@gmail.com>
2023-01-25 15:45:08 -0500

    created my own version of .head

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  let array2 = array.slice(0);
+  let value = array2[0];
+  console.log(value)
+  return value;
+};
+
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
