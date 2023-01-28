67c114148dcb870673b3a303ceddbbdf5fe77b1f
Oscar <omankiewicz77@gmail.com>
2023-01-28 14:50:45 -0500

    made a function to compare arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,10 @@
+let eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🛑🛑🛑 ${arr1} is not equal to ${arr2}`)
+      return false;
+    }
+  }
+  console.log(`✅✅✅ ${arr1} is equal to ${arr2}`)
+  return true;
+}
