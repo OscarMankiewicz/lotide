564ba374f18070aaf55f09555bb00ba4717dd8ea
Oscar <omankiewicz77@gmail.com>
2023-02-01 13:00:12 -0500

    made a function to count how many times a item appers in an object

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
+  }
+};
+
+//allItems: array of strings that needs to be looked through
+//itemsToCount: an object specifying which items need to be counted
+
+const countOnly = function(allItems, itemsToCount) {
+    const results = {}
+
+    for (const item of allItems) {
+        console.log(item);
+        if (itemsToCount[item]) {
+            if(results[item]) {
+                results[item] += 1;
+            } else {
+                results[item] = 1;
+            }
+        }
+    }
+    return results;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
