c9e2e09b971d38c90fd8d047c632e5803c6f40e4
Oscar <omankiewicz77@gmail.com>
2023-02-01 22:02:07 -0500

    made changes to the code so that it can execute properly I hope

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,32 @@
-let assertArraysEqual = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑 ${arr1} is not equal to ${arr2}`)
-      return false;
+const assertEqual = function(actual, expected) {
+  if (Array.isArray(actual) && Array.isArray(expected)) {
+    assertArraysEqual(actual, expected);
+  } else {
+    if (actual === expected) {
+      console.log(`✅✅✅ ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
     }
   }
-  console.log(`✅✅✅ ${arr1} is equal to ${arr2}`)
-  return true;
-}
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  // Check if arrays have the same length
+  if (arr1.length !== arr2.length) {
+    console.log("Arrays are not of equal length.");
+    return;
+  }
+  
+  // Compare elements of both arrays
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log("Arrays are not equal.");
+      return;
+    }
+  }
+  
+  // If all checks passed, arrays are equal
+  console.log("Arrays are equal.");
+};
+
+assertEqual([1,2,3,4,5],[1,2,3,4,5]);
