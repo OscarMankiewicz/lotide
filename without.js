57ed76d04b05b49fc5cd9c9011a725cf4f4d01aa
Oscar <omankiewicz77@gmail.com>
2023-02-03 13:16:42 -0500

    updated code

--- without.js
+++ without.js
@@ -1,13 +1,22 @@
-let assertArraysEqual = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑 ${arr1} is not equal to ${arr2}`)
-      return false;
+let eqArrays = function(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        console.log(false)
+        return false;
+      }
     }
+    console.log(true)
+    return true;
   }
-  console.log(`✅✅✅ ${arr1} is equal to ${arr2}`)
-  return true;
-}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("Arrays are equal.");
+    return;
+  } else {
+    console.log("Arrays are not equal.")
+  }
+};
 
  let without = function(source, itemsToRemove) {
   return source.filter(function(item) {
@@ -15,4 +24,3 @@ let assertArraysEqual = function(arr1, arr2) {
   });
 }
 console.log(without([1,2,3,4,5],[1]))
-
