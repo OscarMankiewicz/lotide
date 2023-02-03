a8dcca8057e398bc318ae95bd96eff4e0c1dfca5
Oscar <omankiewicz77@gmail.com>
2023-02-03 13:13:39 -0500

    Fixed the code again to work hopfully

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,32 +1,21 @@
-const assertEqual = function(actual, expected) {
-  if (Array.isArray(actual) && Array.isArray(expected)) {
-    assertArraysEqual(actual, expected);
-  } else {
-    if (actual === expected) {
-      console.log(`✅✅✅ ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
+let eqArrays = function(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        console.log(false)
+        return false;
+      }
     }
+    console.log(true)
+    return true;
   }
-};
 
 const assertArraysEqual = function(arr1, arr2) {
-  // Check if arrays have the same length
-  if (arr1.length !== arr2.length) {
-    console.log("Arrays are not of equal length.");
+  if (eqArrays(arr1, arr2)) {
+    console.log("Arrays are equal.");
     return;
+  } else {
+    console.log("Arrays are not equal.")
   }
-  
-  // Compare elements of both arrays
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log("Arrays are not equal.");
-      return;
-    }
-  }
-  
-  // If all checks passed, arrays are equal
-  console.log("Arrays are equal.");
 };
 
-assertEqual([1,2,3,4,5],[1,2,3,4,5]);
+assertArraysEqual([1,2,3,4,5],[1,2,3,4,5]);
