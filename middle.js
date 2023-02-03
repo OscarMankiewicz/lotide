b20efc18e9ab8ccf471dde2d0f539cfe63f65914
Oscar <omankiewicz77@gmail.com>
2023-02-03 13:19:42 -0500

    updated code

--- middle.js
+++ middle.js
@@ -1,24 +1,22 @@
 let eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(false)
-      return false;
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        console.log(false)
+        return false;
+      }
     }
+    console.log(true)
+    return true;
   }
-  console.log(true)
-  return true;
-}
 
-let assertArraysEqual = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑 ${arr1} is not equal to ${arr2}`)
-      return false;
-    }
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("Arrays are equal.");
+    return;
+  } else {
+    console.log("Arrays are not equal.")
   }
-  console.log(`✅✅✅ ${arr1} is equal to ${arr2}`)
-  return true;
-}
+};
 
 let middle = function (arr1) {
   let result = []
