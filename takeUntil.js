c42b54ef1a929b2ba542a2ef0f33c0de06d9eaf2
Oscar <omankiewicz77@gmail.com>
2023-02-05 21:10:27 -0500

    linted

--- takeUntil.js
+++ takeUntil.js
@@ -1,46 +1,46 @@
 let eqArrays = function(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(false)
-        return false;
-      }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(false);
+      return false;
     }
-    console.log(true)
-    return true;
   }
+  console.log(true);
+  return true;
+};
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log("Arrays are equal.");
     return;
   } else {
-    console.log("Arrays are not equal.")
+    console.log("Arrays are not equal.");
   }
 };
 
 const takeUntil = function(array, callback) {
-    //array where elements are placed
-    const result = [];
-    //return a slice of the array with elements taken from the beginning
-    for (let e of array) {
-        result.push(e);
-        if (callback(e)) {
-            result.pop(e)
-            break;
-        }
+  //array where elements are placed
+  const result = [];
+  //return a slice of the array with elements taken from the beginning
+  for (let e of array) {
+    result.push(e);
+    if (callback(e)) {
+      result.pop(e);
+      break;
     }
-    //stop when the callback returns a truthy value
-    return result;
-}
+  }
+  //stop when the callback returns a truthy value
+  return result;
+};
 
-const data1= [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1)
-console.log(assertArraysEqual(results1, [1, 2, 5, 7, 2]))
+console.log(results1);
+console.log(assertArraysEqual(results1, [1, 2, 5, 7, 2]));
 
 console.log('---');
 
-const data2= ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2)
-console.log(assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]))
+console.log(results2);
+console.log(assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]));
