1a7890178906df1a6706beb5b2f1af90ac53eb36
Oscar <omankiewicz77@gmail.com>
2023-02-19 11:54:11 -0500

    Exported function and moved test code to test folder

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
-  }
-};
-
 //function needs to take a object and value ✓
 const findKeyByValue = function(obj, value) {
   //scan through the object ✓
@@ -18,20 +10,5 @@ const findKeyByValue = function(obj, value) {
   }
   return undefined;
 };
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
 
-const bestMoviesByGenre = {
-  sci_fi: "Intersteller",
-  action: "The Dark Knight",
-  comedy: "The Hangover"
-};
-  
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-
-assertEqual(findKeyByValue(bestMoviesByGenre, "Intersteller"), "sci_fi");
-assertEqual(findKeyByValue(bestMoviesByGenre, "Terminator 2"), undefined);
+module.exports = findKeyByValue;
