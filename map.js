06de9ecb78840179e981b0d7bf1e0ca0afb03272
Oscar <omankiewicz77@gmail.com>
2023-02-19 12:12:29 -0500

    Exported function and moved test code to test folder

--- map.js
+++ map.js
@@ -1,24 +1,3 @@
-let eqArrays = function(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(false)
-        return false;
-      }
-    }
-    console.log(true)
-    return true;
-  }
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log("Arrays are equal.");
-    return;
-  } else {
-    console.log("Arrays are not equal.")
-  }
-};
-
-const words = ["thorn", "hawkmoon","icebreaker","cloudstrike","riskrunner"]
 const map = function(array, callback) {
     const results = [];
     for (let item of array) {
@@ -26,5 +5,4 @@ const map = function(array, callback) {
     }
     return results;
 }
-const results1 = map(words, word => word[0]);
-console.log(results1);
+module.exports = map;
