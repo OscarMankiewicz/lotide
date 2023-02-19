3573a68bb1f543d45b1d87f715076210a66c7831
Oscar <omankiewicz77@gmail.com>
2023-02-19 11:16:08 -0500

    Added the rest of the object functions

--- index.js
+++ index.js
@@ -3,10 +3,32 @@ const tail = require('./tail');
 const middle = require('./middle');
 const assertEqual = require('./assertEqual');
 const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectEqual = require('./assertObjectEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 module.exports = {
     head,
     tail,
     middle,
     assertEqual,
     assertArraysEqual,
+    assertObjectEqual,
+    countLetters,
+    countOnly,
+    eqArrays,
+    eqObjects,
+    findKey,
+    findKeyByValue,
+    flatten,
+    letterPositions,
+    takeUntil,
+    without,
 };
