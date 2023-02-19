9f160f3c57278e7e0bd38ddca86ea610c9dd0aae
Oscar <omankiewicz77@gmail.com>
2023-02-19 12:17:50 -0500

    Exported function and moved test code to test folder

--- takeUntil.js
+++ takeUntil.js
@@ -1,23 +1,3 @@
-let eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(false);
-      return false;
-    }
-  }
-  console.log(true);
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log("Arrays are equal.");
-    return;
-  } else {
-    console.log("Arrays are not equal.");
-  }
-};
-
 const takeUntil = function(array, callback) {
   //array where elements are placed
   const result = [];
@@ -33,14 +13,4 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-console.log(assertArraysEqual(results1, [1, 2, 5, 7, 2]));
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-console.log(assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]));
+module.exports = takeUntil;
