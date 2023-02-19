4e44cecf32b6dac093e33cdc4ba182c9eff03217
Oscar <omankiewicz77@gmail.com>
2023-02-19 11:16:39 -0500

    removed unnecessary comment

--- test/middleTest.js
+++ test/middleTest.js
@@ -14,19 +14,3 @@ describe("middle", () => {
         assert.deepEqual(middle([1, 2]), [1,2]);
     });
 });
-
-
-/*
-describe("#head", () => {
-    it("returns 1 for [1, 2, 3]", () => {
-        assert.strictEqual(head([1, 2, 3]), 1);
-    });
-
-    it("returns 5 for ['5']", () => {
-        assert.strictEqual(head(['5']), '5');
-    });
-});
-*/
-//middle([1,2,3,4,5])
-//middle([1,2,3,4])
-//middle([1,2])
