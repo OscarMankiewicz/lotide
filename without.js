83f7184f12fb2b70815ed5e7465da2ce00e44b84
Oscar <omankiewicz77@gmail.com>
2023-02-19 12:20:39 -0500

    Exported function and moved test code to test folder

--- without.js
+++ without.js
@@ -1,26 +1,7 @@
-let eqArrays = function(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(false)
-        return false;
-      }
-    }
-    console.log(true)
-    return true;
-  }
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log("Arrays are equal.");
-    return;
-  } else {
-    console.log("Arrays are not equal.")
-  }
-};
-
- let without = function(source, itemsToRemove) {
+let without = function(source, itemsToRemove) {
   return source.filter(function(item) {
     return !itemsToRemove.includes(item);
   });
 }
-console.log(without([1,2,3,4,5],[1]))
+
+module.exports = without;
